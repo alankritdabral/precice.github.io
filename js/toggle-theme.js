3c9a67551385b11eaf9aa503745c9d3747149b56
alankritdabral <dabralalankrit@gmail.com>
2026-04-01 20:19:30 +0530

    added dark theme

---
+++ js/toggle-theme.js
@@ -0,0 +1,30 @@
+const toggleBtn = document.getElementById("theme-toggle");
+const icon = document.getElementById("theme-icon");
+
+// Load saved theme
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme) {
+  document.documentElement.setAttribute("data-theme", savedTheme);
+  updateIcon(savedTheme);
+} else {
+  const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const defaultTheme = prefersDark ? "dark" : "light";
+  document.documentElement.setAttribute("data-theme", defaultTheme);
+  updateIcon(defaultTheme);
+}
+toggleBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  let currentTheme = document.documentElement.getAttribute("data-theme");
+  let newTheme = currentTheme === "dark" ? "light" : "dark";
+
+  document.documentElement.setAttribute("data-theme", newTheme);
+  localStorage.setItem("theme", newTheme);
+
+  updateIcon(newTheme);
+});
+
+function updateIcon(theme) {
+  icon.className = theme === "dark" ? "fas fa-sun" : "fas fa-moon";
+}
